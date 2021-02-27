712845f260347cff18839b5b368314e2df469b3a
Khaw100 <rakhawiratama10@gmail.com>
2021-02-27 23:17:11 +0700

    first commit

---
+++ assets/index.js
@@ -0,0 +1,44 @@
+console.log("Test")
+const khaw = {
+ age: 20;
+ nim: 18218004;
+}
+
+const bahanMasak = ["apel","garam"]
+
+console.log(khaw.age)
+
+console.log(bahanMasak[1])
+
+let a = 4
+let b = 7
+let c = a % b
+console.log(c)
+let h = -1
+
+if(a>5) {
+ h = 10
+ console.log("nice")
+} else {
+ h = 3
+ console/log("weebs")
+}
+
+let h = (a>5) ? 10 : 15
+
+for(let a=0; a<10; a++) {
+ console.log(a)
+}
+
+const a = ["apel","anggur","amer"]
+for(const val of a) {
+ console.log(a)
+}
+
+for(const val in a) {
+ console.log(a)
+}
+const multiply = (a,b) => {
+ a = a*3
+ b = b-7 
+}
